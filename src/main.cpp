16aa00271cabcd1d7ff5448a3c4bba0b9faf35e4
Kaede <the.devils.emperor@gmail.com>
2025-10-23 13:34:54 +0200

    Establishing a working base. General CMake. Separation of areas of work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
 //
 // Created by Kaede on 2025-10-23.
 //
+
+#include <SFML/Graphics.hpp>
+
+int main(){
+    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
+    window.setFramerateLimit(30);
+    while (window.isOpen()){
+        while (const std::optional event = window.pollEvent()){
+            if (event->is<sf::Event::Closed>()){
+                window.close();
+            }
+        }
+        window.clear();
+        window.display();
+    }
+}
